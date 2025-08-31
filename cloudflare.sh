435c07a3bb860b9e56a62a3a6503711840b0af10
miraonway <miraonway@gmail.com>
2025-08-31 18:34:29 +0300

    Refactor cloudflare.sh for IP whitelisting

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,7 +1,20 @@
-for i in `curl https://www.cloudflare.com/ips-v4`; do iptables -I INPUT -p tcp -m multiport --dports http,https -s $i -j ACCEPT; done
-for i in `curl https://www.cloudflare.com/ips-v6`; do ip6tables -I INPUT -p tcp -m multiport --dports http,https -s $i -j ACCEPT; done
+#!/bin/bash
 
-iptables -A INPUT -p tcp -m multiport --dports http,https -j DROP
-ip6tables -A INPUT -p tcp -m multiport --dports http,https -j DROP
+# Exit on error and undefined variables
+sudo set -eu
 
-rm cloudflare.sh
+# IPv4 rules
+for ip in $(curl -s https://www.cloudflare.com/ips-v4); do
+    sudo iptables -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT
+done
+
+# IPv6 rules (only if IPv6 is enabled)
+for ip in $(curl -s https://www.cloudflare.com/ips-v6); do
+    sudo ip6tables -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT
+done
+
+# Block all other HTTP/HTTPS traffic
+sudo iptables -A INPUT -p tcp -m multiport --dports http,https -j DROP
+sudo ip6tables -A INPUT -p tcp -m multiport --dports http,https -j DROP
+
+echo "Cloudflare IPs whitelisted and other HTTP/S traffic blocked"
