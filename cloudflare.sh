f38ee93f6066423bde747a808ba25c2226f38ff2
miraonway <miraonway@gmail.com>
2025-08-31 18:35:32 +0300

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Exit on error and undefined variables
-sudo set -eu
+set -eu
 
 # IPv4 rules
 for ip in $(curl -s https://www.cloudflare.com/ips-v4); do
-    sudo iptables -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT
+    iptables -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT
 done
 
 # IPv6 rules (only if IPv6 is enabled)
 for ip in $(curl -s https://www.cloudflare.com/ips-v6); do
-    sudo ip6tables -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT
+    ip6tables -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT
 done
 
 # Block all other HTTP/HTTPS traffic
-sudo iptables -A INPUT -p tcp -m multiport --dports http,https -j DROP
-sudo ip6tables -A INPUT -p tcp -m multiport --dports http,https -j DROP
+iptables -A INPUT -p tcp -m multiport --dports http,https -j DROP
+ip6tables -A INPUT -p tcp -m multiport --dports http,https -j DROP
 
 echo "Cloudflare IPs whitelisted and other HTTP/S traffic blocked"
