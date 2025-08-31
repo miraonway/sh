19fa4536b1862d73ce935b0f4e7b9aa4ce8db107
miraonway <miraonway@gmail.com>
2025-08-31 18:30:51 +0300

    Push cloudflare.sh

---
+++ cloudflare.sh
@@ -0,0 +1,5 @@
+for i in `curl https://www.cloudflare.com/ips-v4`; do iptables -I INPUT -p tcp -m multiport --dports http,https -s $i -j ACCEPT; done
+for i in `curl https://www.cloudflare.com/ips-v6`; do ip6tables -I INPUT -p tcp -m multiport --dports http,https -s $i -j ACCEPT; done
+
+iptables -A INPUT -p tcp -m multiport --dports http,https -j DROP
+ip6tables -A INPUT -p tcp -m multiport --dports http,https -j DROP
