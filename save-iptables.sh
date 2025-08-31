eea4a05f5a477583f7aff188ade3a5be621023a3
miraonway <miraonway@gmail.com>
2025-08-31 18:44:08 +0300

    Update save-iptables.sh

--- save-iptables.sh
+++ save-iptables.sh
@@ -1,3 +1,9 @@
 sudo apt-get -y install iptables-persistent
+
+# For old systems
 sudo /etc/init.d/iptables-persistent save 
 sudo /etc/init.d/iptables-persistent reload
+
+# For new systems
+sudo netfilter-persistent save
+sudo netfilter-persistent reload
